49563063f28e5b6ae62def80b711a62b11c2a76e
babaquero07 <brayanalexanderb@gmail.com>
2023-03-31 08:14:37 -0500

    defineProps - Script Setup

--- src/shared/components/NavBar.vue
+++ src/shared/components/NavBar.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 
+interface Props {
+    title?: string;
+}
+
+const props = defineProps<Props>()
+
 </script>
 
 <template>
     <nav>
         <img src="@/assets/logo.svg" alt="Vue Logo" height="25" width="25">
-        <span>Algo</span>
+        <span>{{ props.title || 'Algo' }}</span>
 
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
