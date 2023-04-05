0a08d2ae2162e80634847379e5df3160dbec90dd
babaquero07 <brayanalexanderb@gmail.com>
2023-04-05 15:57:11 -0500

    useQuery

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
 // import { ref } from 'vue'
-// import breakingBadApi from '@/api/breakingBadApi'
-// import type { CharacterQuote } from '@/characters/interfaces/character'
-import { useQuotes } from '@/characters/composables/useQuotes'
-
+import breakingBadApi from '@/api/breakingBadApi'
+import type { CharacterQuote } from '@/characters/interfaces/character'
+// import { useQuotes } from '@/characters/composables/useQuotes'
+import { useQuery } from '@tanstack/vue-query'
+//! 1- Nomal suspense
 // const { data } = await breakingBadApi.get<CharacterQuote[]>('/5');
 // const quotes = ref<CharacterQuote[]>(data)
-const { isLoading, quotes, hasError, errorMessage } = useQuotes()
+
+//! 2- Composable functions
+// const { isLoading, quotes, hasError, errorMessage } = useQuotes()
+
+const getQuotesSlow = async (): Promise<CharacterQuote[]> => {
+  const { data } = await breakingBadApi.get<CharacterQuote[]>('/5')
+
+  return data
+}
+//! 3 TanStack Query
+const { isLoading, isError, data: quotes, error } = useQuery(['quotes'], getQuotesSlow)
 </script>
 
 <template>
   <h1 v-if="isLoading">Loading...</h1>
-  <h1 v-if="hasError">{{ errorMessage }}</h1>
+  <h1 v-if="isError">{{ error }}</h1>
   <ul>
     <li v-for="quote of quotes" :key="quote.author">
       <p>{{ quote.quote }}</p>
