ead2cd671fd9abbbd383cc7410f96b2495842fa5
babaquero07 <brayanalexanderb@gmail.com>
2023-04-05 14:52:31 -0500

    Composable functions - useQuotes con estado global

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import breakingBadApi from '@/api/breakingBadApi';
-import type { CharacterQuote } from '@/characters/interfaces/character';
-import { useQuotes } from '@/characters/composables/useQuotes';
+// import { ref } from 'vue'
+// import breakingBadApi from '@/api/breakingBadApi'
+// import type { CharacterQuote } from '@/characters/interfaces/character'
+import { useQuotes } from '@/characters/composables/useQuotes'
 
 // const { data } = await breakingBadApi.get<CharacterQuote[]>('/5');
 // const quotes = ref<CharacterQuote[]>(data)
-const { isLoading, quotes } = useQuotes();
-
+const { isLoading, quotes, hasError, errorMessage } = useQuotes()
 </script>
 
 <template>
-    <h1 v-if="isLoading">Loading...</h1>
-    <ul>
-        <li v-for="quote of quotes" :key="quote.author">
-            <p>{{ quote.quote }}</p>
-            <span>{{ quote.author }}</span>
-        </li>
-    </ul>
+  <h1 v-if="isLoading">Loading...</h1>
+  <h1 v-if="hasError">{{ errorMessage }}</h1>
+  <ul>
+    <li v-for="quote of quotes" :key="quote.author">
+      <p>{{ quote.quote }}</p>
+      <span>{{ quote.author }}</span>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
--- src/characters/composables/useQuotes.ts
+++ src/characters/composables/useQuotes.ts
@@ -1,17 +1,39 @@
-import { ref } from "vue";
-import breakingBadApi from "@/api/breakingBadApi";
-import type { CharacterQuote } from "../interfaces/character";
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import breakingBadApi from '@/api/breakingBadApi'
+import type { CharacterQuote } from '../interfaces/character'
+
+const quotes = ref<CharacterQuote[]>([])
+const isLoading = ref<boolean>(true)
+const hasError = ref<boolean>(false)
+const errorMessage = ref<string>()
 
 export const useQuotes = () => {
-    const quotes = ref<CharacterQuote[]>([]);
-    const isLoading = ref<boolean>(true);
+  onMounted(() => {
+    loadQuotes()
+  })
 
-    breakingBadApi.get<CharacterQuote[]>('/5')
-        .then(({ data }) => {
-            console.log(data);
-            quotes.value = data;
-            isLoading.value = false;
-        })
+  const loadQuotes = async () => {
+    if (quotes.value.length > 0) return
 
-  return { quotes, isLoading }
+    isLoading.value = true
+    try {
+      const { data } = await breakingBadApi.get<CharacterQuote[]>('/5')
+
+      quotes.value = data
+    } catch (error) {
+      console.log(error)
+      hasError.value = true
+
+      if (axios.isAxiosError(error)) {
+        return (errorMessage.value = error.message)
+      }
+
+      errorMessage.value = JSON.stringify(error)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return { quotes, isLoading, hasError, errorMessage }
 }
